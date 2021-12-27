137815e4c1e55f99b317b709f39eb07cef9ad7a1
danielcr0995 <daniel.correa.1995@gmail.com>
2021-12-26 20:29:38 -0500

    added code to get files from server side

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const app= express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static('public'));
+
 app.get('/', function(req,res){
-    res.send('hola');
+    res.sendFile(__dirname+ '/signup.html');
 })
 
 
